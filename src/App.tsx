8d346d9e8a84f26f43b9f994208373fbd9e615a3
oatBananazaa <kritsana_ju64@live.rmutl.ac.th>
2026-02-02 14:22:05 +0700

    frist commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Main_page from "./components/Mainpage/mainpage";
+import Navbar from "./components/Nva/Navbar";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Main_page />
+    </>
+  );
+}
